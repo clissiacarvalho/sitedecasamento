24658b5b7e7f1a7b8410330e55388e576d0f186f
MARI\maria <mariailzaraquino@gmail.com>
2023-10-18 18:11:31 -0300

    Blog finalizado, correção do logo e do caminho do css no header de todas as páginas

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 const daysElement = document.querySelector("[data-days]")
 const hoursElement = document.querySelector("[data-hours]")
 const minutesElement = document.querySelector("[data-minutes]")
@@ -28,3 +29,34 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000);
+
+/* JS BLOG */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
